c97491787ee00d14d9476b966bc94a44bdbad1b0
Nazar Sabadash <nazarii.sabadash@gmail.com>
2024-01-05 20:48:18 +0000

    Working with State. Updated CSS styles and dialog window.

--- starting-project/src/components/PostsList.jsx
+++ starting-project/src/components/PostsList.jsx
@@ -2,11 +2,17 @@ import { useState } from 'react';
 
 import Post from './Post';
 import NewPost from './NewPost';
+import Modal from './Modal';
 import classes from './PostsList.module.css';
 
 function PostsList() {
+  const [ modalIsVisible, setModalIsVisible] = useState(true);
   const [ enteredBody, setEnteredBody] = useState('');
   const [ enteredAuthor, setEnteredAuthor] = useState(''); 
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
 
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
@@ -18,7 +24,15 @@ function PostsList() {
 
   return (
     <>
-      <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
+      {modalIsVisible ? (
+        <Modal onClose={hideModalHandler}>
+          <NewPost 
+          onBodyChange={bodyChangeHandler} 
+          onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      ) : null}
+      
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody}/>
         <Post author="Mike" body="Hey ya!"/>
