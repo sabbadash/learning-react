c9fd45301af6ad81cbb807721822caafb6e0500e
Nazar Sabadash <nazarii.sabadash@gmail.com>
2024-01-10 15:19:50 +0000

    Added routes and navigation. Changed styles in NewPost.module.css

---
+++ starting-project/src/routes/NewPost.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import classes from './NewPost.module.css';
+import Modal from '../components/Modal';
+
+function NewPost({ onAddPost }) {
+  const [ enteredBody, setEnteredBody] = useState('');
+  const [ enteredAuthor, setEnteredAuthor] = useState(''); 
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();   //preventing sending http request to server and reloading the page
+
+    const postData = {
+      body: enteredBody,
+      author: enteredAuthor,
+    };
+    onAddPost(postData);
+    onCancel();
+  }
+
+  return (
+    <Modal>
+      <form className={classes.form} onSubmit={submitHandler}>
+        <p>
+          <label htmlFor="body">Text</label>
+          <textarea id="body" required rows={3} onChange={bodyChangeHandler}/>
+        </p>
+        <p>
+          <label htmlFor="name">Your name</label>
+          <input type="text" id="name" required onChange={authorChangeHandler} />
+        </p>
+        <p className={classes.actions}>
+          <Link to=".." type='button'>
+            Cancel
+          </Link>
+          <button type='submit'>Submit</button>
+        </p>
+      </form>
+    </Modal>
+  );
+}
+
+export default NewPost;
